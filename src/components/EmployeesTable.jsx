0f5ed826072bc2f0e8ea78ec6d9793760f7d4ccc
Wajeeh <wajeeh.zantout@gmail.com>
2018-06-21 17:38:41 +0300

    feat: add react table

--- src/components/EmployeesTable.jsx
+++ src/components/EmployeesTable.jsx
@@ -3,19 +3,77 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
-class EmployeesTable extends Component<{}, {}> {
+type Props = {
+  employeesQuery: Object,
+};
+
+class EmployeesTable extends Component<Props, {}> {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-  render() {
+  renderTable() {
+    const { employees, loading } = this.props.employeesQuery;
     return (
-      <div>
-        <p>Employees Table</p>
-      </div>
+      <ReactTable
+        data={employees}
+        loading={loading}
+        columns={[
+          {
+            Header: 'Employees Table',
+            columns: [
+              {
+                Header: 'First Name',
+                accessor: 'firstName',
+              },
+              {
+                Header: 'Last Name',
+                accessor: 'lastName',
+              },
+              {
+                Header: 'Job Title',
+                accessor: 'jobTitle',
+              },
+              {
+                Header: 'Phone Number',
+                accessor: 'phoneNumber',
+              },
+              {
+                Header: 'Edit',
+                width: 90,
+                Filter: () => null,
+                Cell: () => null,
+              },
+              {
+                Header: 'Remove',
+                width: 90,
+                Filter: () => null,
+                Cell: () => null,
+              },
+            ],
+          },
+        ]}
+        filterable
+        defaultFilterMethod={(filter, row) => {
+          const id = filter.pivotId || filter.id;
+          return row[id]
+            ? String(row[id])
+                .toLowerCase()
+                .startsWith(filter.value.toLowerCase())
+            : true;
+        }}
+        showPageSizeOptions={false}
+        className="-striped -highlight"
+      />
     );
+  }
+
+  render() {
+    return <div className="container">{this.renderTable()}</div>;
   }
 }
 
