7be17a8763bb6f4f9a32251ad6078137d105b692
Wajeeh <wajeeh.zantout@gmail.com>
2018-06-21 17:18:35 +0300

    feat: add get employees query

--- src/components/EmployeesTable.jsx
+++ src/components/EmployeesTable.jsx
@@ -1,11 +1,34 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const EmployeesTable = () => (
-  <div>
-    <p>Employees Table</p>
-  </div>
-);
+class EmployeesTable extends Component<{}, {}> {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default EmployeesTable;
+  render() {
+    return (
+      <div>
+        <p>Employees Table</p>
+      </div>
+    );
+  }
+}
+
+const EMPLOYEES_QUERY = gql`
+  query EmployeesQuery {
+    employees {
+      id
+      firstName
+      lastName
+      jobTitle
+      phoneNumber
+    }
+  }
+`;
+
+export default graphql(EMPLOYEES_QUERY, { name: 'employeesQuery' })(EmployeesTable);
